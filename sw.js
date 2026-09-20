87efd3ff1dea3fc1404f15c7758839242ddb980f
Nsstik <nastena-528@mail.ru>
2026-09-20 14:07:23 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+// Офлайн-режим: сначала берём свежую версию из сети, без интернета отдаём сохранённую.
+const CACHE = 'domashka-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './favicon.svg',
+  './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        if (res && (res.ok || res.type === 'opaque')) {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((hit) => hit || caches.match('./index.html')))
+  );
+});
