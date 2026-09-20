6b8e85d4e0f9cc8e1f5dbac99e418156b5fb3782
Nsstik <nastena-528@mail.ru>
2026-09-20 14:42:18 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Настройки облака для синхронизации списка между устройствами.
+// Эти значения не секретные: данные защищают правила доступа в Firestore.
+window.DOMASHKA_FIREBASE = {
+  apiKey: "AIzaSyBMgCB__3T6HoSKVnwy4FhmGSJXnCYA-iQ",
+  authDomain: "domashka-78f64.firebaseapp.com",
+  projectId: "domashka-78f64",
+  storageBucket: "domashka-78f64.firebasestorage.app",
+  messagingSenderId: "57850625195",
+  appId: "1:57850625195:web:a19b0403ecc94743e60bee",
+  measurementId: "G-PVXELSVPS9"
+};
